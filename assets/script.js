7889fa143ef5844d5d0bc991aa09441d22257426
Lee-moran <leemoran06@hotmail.com>
2022-02-07 19:35:41 +0000

    question function added to js and applied math random

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,40 @@
 const startButton = document.getElementById('start-btn')
 const questionBoxElement = document.getElementById('question-box')
 
+const shuffledQuestions, currentQuestionIndex
+
+
 startButton.addEventListener('click', startGame);
 
 function startGame() {
     console.log('started');
     startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0; 
     questionBoxElement.classList.remove('hide');
+    setNextQuestion()
 
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+
+function showQuestion(question)
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'what is 2 + 2',
+        answer: [
+             {text: '4', correct: true},
+             {text: '22', correct: false},
+             {text: '2', correct: false},
+             {text: '-2', correct: false} 
+        ]
+    }
+]
